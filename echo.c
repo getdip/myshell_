05d6da1c0fb3ae0370b07a12eda516798d4a3bdf
Dip Chakraborty <70464668+getdip@users.noreply.github.com>
2026-07-30 12:49:21 +0530

    Update echo.c

--- echo.c
+++ echo.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 void echo(char* buf[]){
-    //exit_val=0;
+    
 
     if(!strcmp(buf[1],"$SHELL")){       //Prints the SHELL's path
         exit_val=0;
-        //getcwd(shell_path,sizeof(shell_path));
+        
         printf("%s\n",shell_path);
     }
 
     else if(!strcmp(buf[1],"$?")){      //Prints the exit value of the last executed command
-       // exit_val=0;
+       
         printf("%d\n",exit_val);        
         exit_val=0;
     }
