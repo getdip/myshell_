707aafae03f63b21647ce41baf80d86be0dc6a34
Dip Chakraborty <70464668+getdip@users.noreply.github.com>
2026-07-30 12:57:20 +0530

    Update int_cmd.c

--- int_cmd.c
+++ int_cmd.c
@@ -3,14 +3,14 @@
 void int_cmd(char *buf[]){
     exit_val=0;
 
-    // cd 
-    // pwd
-    // jobs
-    // fg 
-    // bg
+    /** 
+        Implementation of commands--
+        cd, pwd, jobs,fg, bg
+    */
     
-    // cd ==> change directory as per user mentioned path. If path not provided changes to default /home/<username> 
-
+    /**
+        cd ==> change directory as per user mentioned path. If path not provided changes to default /home/<username> 
+    */
     if(!strcmp(buf[0],"cd")){
         
         if((buf[1]==NULL)){
@@ -20,7 +20,10 @@ void int_cmd(char *buf[]){
 
     }
 
-    //pwd  ==> Prints the present working directory on the terminal
+    
+    /**
+        pwd  ==> Prints the present working directory on the terminal
+    */
     
     if(!strcmp(buf[0],"pwd")){ 
         char dir_name[1024];
@@ -29,7 +32,10 @@ void int_cmd(char *buf[]){
         printf("\n");
     }
 
-    //jobs ==> display the processes in background and stopped processes 
+    
+    /**
+        jobs ==> display the processes in background and stopped processes 
+    */
 
     if(!strcmp(buf[0],"jobs")){
 
@@ -49,11 +55,13 @@ void int_cmd(char *buf[]){
 
     }
 
-    //fg ==> resumes the recently stopped in foreground
+    
+    /**
+        fg ==> resumes the recently stopped in foreground
+    */
 
     if(!strcmp(buf[0],"fg")){
 
-        //fg_running=1;
         if(j_head==NULL)printf("No suspended process to resume\n");
 
         else{
@@ -76,8 +84,9 @@ void int_cmd(char *buf[]){
      }
     }
 
-    //bg ==> resumes the recently stopped in background
-
+    /**
+        bg ==> resumes the recently stopped in background
+    */
     if(!strcmp(buf[0],"bg")){
 
         if(j_head==NULL)printf("No suspended process to resume\n");
