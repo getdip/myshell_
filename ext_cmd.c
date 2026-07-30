876ed82e5d4c3070d95bac1dfe084cb1449cfaa6
Dip Chakraborty <70464668+getdip@users.noreply.github.com>
2026-07-30 13:21:35 +0530

    Update ext_cmd.c

--- ext_cmd.c
+++ ext_cmd.c
@@ -10,28 +10,30 @@ int bg_job_cnt=0;
 
 void ext_cmnd(char *buf[]) {
 
-    //exit_val=0;
         int x;
         pid_t ret = fork();
-        
-        if(ret == 0){       // Child process         
+
+    /* Child process */
+        if(ret == 0){                
             
            setpgid(0,0);
-            if(run_bg==0){ // Running in foreground
-
-                //This is from the context of a child process, previously defined( in main- parent process) SIGINT & SIGTSTP was modified to redisplay the prompt
-                //But here we need to define what action when ctrl+C(SIGINT) and ctrl+Z(SIGTSTP) are pressed from keyboard. SIGINT & SIGTSTP should have default behaviour. 
-
-                tcsetpgrp(tty_fd, getpid());            //Terminal control goes from myshell process(parent) to the child process
+            /*Running in foreground*/
+            if(run_bg==0){ 
+                /**
+                    This is from the context of a child process, previously defined( in main- parent process) SIGINT & SIGTSTP 
+                    was modified to redisplay the prompt.But here we need to define what action when ctrl+C(SIGINT) and ctrl+Z(SIGTSTP) are 
+                    pressed from keyboard. SIGINT & SIGTSTP should have default behaviour. 
+                */
+                tcsetpgrp(tty_fd, getpid());  /*Terminal control goes from myshell process(parent) to the child process*/
                 struct sigaction signal_fgnd;
-                signal_fgnd.sa_handler = SIG_DFL;       // SIGINT - Terminates the process , SIGTSTP - Stops the process
+                signal_fgnd.sa_handler = SIG_DFL;  /* SIGINT - Terminates the process , SIGTSTP - Stops the process*/
                 sigemptyset(&signal_fgnd.sa_mask);
                 sigaction(SIGTSTP,&signal_fgnd,NULL);
                 sigaction(SIGINT,&signal_fgnd,NULL);
                 
             }
 
-            // Running in background
+            /* Running in background */
             if(execvp(buf[0],buf)==-1){
                 perror("Invalid command");
                 exit(127);
@@ -39,11 +41,12 @@ void ext_cmnd(char *buf[]) {
             
         }
 
-        else if(ret>0) {       // Parent Process
+    /* Parent Process */
+        else if(ret>0) {       
             setpgid(ret,ret);
             int status;
                 
-            if (run_bg==0){     // for foreground process
+            if (run_bg==0){     /* Foreground process */
                  
                 tcsetpgrp(tty_fd,ret);
            
@@ -51,25 +54,24 @@ void ext_cmnd(char *buf[]) {
             
             if (WIFEXITED(status)) {
                 exit_val=WEXITSTATUS(status);
-                if(exit_val == 0) printf("Process %d terminated\n",ret);  // Child terminates gracefully      
+                if(exit_val == 0) printf("Process %d terminated\n",ret);  /* Child terminates gracefully */   
                  
             }
             else if (WIFSIGNALED(status)){ 
-                //remove_pid();
-                printf("Process %d has terminatd with a signal %d\n",ret,(exit_val=WTERMSIG(status)));  // Child terminates with a signal
+                printf("Process %d has terminatd with a signal %d\n",ret,(exit_val=WTERMSIG(status)));  /* Child terminates with a signal */
                 exit_val+=128;
             }
             else if (WIFSTOPPED(status)){
                 insert_pid(ret);
-                printf("\nPID:%d      [stopped]\n",ret);    // Child is stopped
+                printf("\nPID:%d      [stopped]\n",ret);    /* Child is stopped */
                 exit_val+=128;
             } 
-            tcsetpgrp(tty_fd,shell_pgid);                   // Terminal control comes backto myshell process
+            tcsetpgrp(tty_fd,shell_pgid);                   /* Terminal control comes backto myshell process */
             tcsetattr(tty_fd,TCSADRAIN,&shell_modes);
             } 
 
             else if(run_bg ==1){                            
-                insert_pid(ret);                            //Background jobs are also added into the job queue 
+                insert_pid(ret);                            /* Background jobs are also added into the job queue */
                 bg_jobs[bg_job_cnt++]=ret;
                 printf("[%d]%d\n",bg_job_cnt,ret);
             }
